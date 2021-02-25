48ec22e7897a6f78aafe1e16b89027598dfb2ca3
skrevolve <sukyu0919@naver.com>
2021-02-25 18:06:42 +0900

    upload

---
+++ server.js
@@ -0,0 +1,26 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+
+http.createServer( (request, response) => {
+    
+    var pathname = url.parse(request.url).pathname
+
+    console.log('pathname: ' + pathname)
+	
+  	// pathname으로 router 분기함 
+    if (pathname == '/') {
+        pathname = '/views/index.html'
+    }
+
+    fs.readFile(pathname.substr(1), (err, data) => {
+        if (err) {
+            console.log(err)
+            response.writeHead(404, {'Content-Type': 'text/html'})
+        } else {
+            response.writeHead(200, {'Content-Type': 'text/html'})
+            response.write(data.toString())
+        }
+        response.end()
+    })
+}).listen(3000)
